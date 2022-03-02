4416ff426cc4ed15f81e5ce9564ec0629fef2485
lhn <namluong16@gmail.com>
2022-03-02 10:50:54 +0700

    Demo3

--- test1/src/main/java/com/mycompany/test1/PrimaryController.java
+++ test1/src/main/java/com/mycompany/test1/PrimaryController.java
@@ -3,22 +3,28 @@ package com.mycompany.test1;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class PrimaryController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    @FXML private TextField heightTxt;
+    @FXML private TextField weightTxt;
+    @FXML private Label bmi;
+    private double height = Double.parseDouble(heightTxt.getText());
+    private double weight = Double.parseDouble(weightTxt.getText());
+    private double bmiResult;
     
-    public void Controller(ActionEvent event){
-<<<<<<< HEAD
-    int a = 10;
-    int b = 20;
-=======
-    int a = 100;
-    int b = 200;
->>>>>>> main
-    int c = a + b;
+    public void bmiHandler(ActionEvent event){
+        bmiResult = weight / (Math.pow(height, 2));
+        
+        if(bmiResult < 18.5)
+            bmi.setText("Gay");
+        else if(bmiResult < 25)
+            bmi.setText("Binh thuong");
+        else{
+            bmi.setTextFill(Color.RED);
+            bmi.setText("Thua can");
+        }
     }
 }
